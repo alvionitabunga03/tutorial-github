1de075d25f3ae5589d23bb82f4964cc2ca9427ec
alvionitabunga03 <48240809+alvionitabunga03@users.noreply.github.com>
2019-04-12 09:02:41 +0700

    Add files via upload

---
+++ minggu5/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/	
+
+Route::get('/produk', 'produkController@index');
+
+Route::get('/halaman',function() 
+{
+	$title = 'Harry Pooter';
+	$konten = 'harry potter and the deathly hallows: part2';
+	return view('konten/halaman',compact('title','konten'));
+});
+
+Route::get('/pelanggan', 'pelangganController@index');
+
+Route::resource('produk','produkController');
